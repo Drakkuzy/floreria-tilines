2f965b3390f342c0cd22071e0d5ce56679ece3f5
Drakkzy <42106487+Drakkuzy@users.noreply.github.com>
2023-07-06 18:33:39 -0400

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Producto)
-admin.site.register(Venta)
-admin.site.register(Detalle)
-admin.site.register(UsuarioSuscrito)
