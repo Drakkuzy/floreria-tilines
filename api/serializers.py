0a0e830307995001b0d3240023eafb695f52a4e6
Drakkzy <42106487+Drakkuzy@users.noreply.github.com>
2023-07-06 18:33:28 -0400

    Delete api directory

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from core.models import Producto
-
-class ProductoSerializer(ModelSerializer):
-    class Meta:
-        model = Producto
-        exclude = ['stock']
-        
